05e7ec71e1c8453483c2dc273ff38dbd13dd500e
Aniket <anklet.devil09@gmail.com>
2023-08-03 23:14:49 +0530

    LEVEL_ORDER_TRAVERSAL

---
+++ Level Order Traversal.java
@@ -0,0 +1,47 @@
+
+
+class TreeNode{
+	int data;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int data){
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Solution{
+	public List<List<Integer>> levelorderTraversal(TreeNode root){
+	List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelNodes = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelNodes.add(node.data);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(levelNodes);
+        }
+
+        return result;
+    }
+}
