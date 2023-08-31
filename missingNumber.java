60f48838e629f8cb61e2502f6a45767716cc1d41
Aniket <anklet.devil09@gmail.com>
2023-08-31 17:02:23 +0530

    modified:   missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -1,13 +1,22 @@
-import java.util.Arrays;
-
+// USING CYCLIC SORT TIMECOMPLEXITY O(N) AND SPACE COMPLEXITY O(1)
 class Solution {
     public int missingNumber(int[] nums) {
-        int res=0;
-        for(int i=1;i<=nums.length;i++){
-            if(!Arrays.asList(nums).contains(i)){
-                res=i;
+        int i=0;
+        while(i < nums.length){
+            if(nums[i] < nums.length && nums[i]!=i){
+                int temp = nums[nums[i]];
+                nums[nums[i]] = nums[i];
+                nums[i]=temp;
+            }else{
+                i++;
             }
         }
-        return res;
+        // System.out.println(Arrays.toString(nums));
+        for(int j=0;j<nums.length;j++){
+            if(j!=nums[j]){
+                return j;
+            }
+        }
+        return nums.length;
     }
 }
