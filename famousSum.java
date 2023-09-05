07563314194f8d33b8013ce009ece0181ee23ed3
Aniket <anklet.devil09@gmail.com>
2023-09-05 14:24:48 +0530

    famousSum.java

---
+++ famousSum.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class Main {
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        String f = s.next();
+        int n1 = s.nextInt();
+        int n0=0;
+        char[] c = f.toCharArray();
+        for(char i:c){
+            n0+=Character.getNumericValue(i);
+            n0%=9;
+        }
+        int n = 9;
+        if(((n0%n)*(n1%n))%n==0)
+            System.out.println(n);
+        else
+            System.out.println(((n0%n)*(n1%n))%n);
+    }
+}
