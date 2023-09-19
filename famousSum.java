552069439d04754acca80b5621e6008a4cc1777a
Aniket <anklet.devil09@gmail.com>
2023-09-19 12:33:54 +0530

    Jumping on the clouds

--- famousSum.java
+++ famousSum.java
@@ -1,21 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+
 class Main {
-    public static void main(String args[] ) throws Exception {
+    public static void main(String args[]) throws Exception {
         Scanner s = new Scanner(System.in);
         String f = s.next();
         int n1 = s.nextInt();
-        int n0=0;
+        int n0 = 0;
         char[] c = f.toCharArray();
-        for(char i:c){
-            n0+=Character.getNumericValue(i);
-            n0%=9;
+        for (char i : c) {
+            n0 += Character.getNumericValue(i);
+            n0 %= 9;
         }
         int n = 9;
-        if(((n0%n)*(n1%n))%n==0)
+        if (((n0 % n) * (n1 % n)) % n == 0)
             System.out.println(n);
         else
-            System.out.println(((n0%n)*(n1%n))%n);
+            System.out.println(((n0 % n) * (n1 % n)) % n);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ jumpingOnTheClouds.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        
+        while(tc-- > 0){
+            int n=sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            System.out.println(noOfJumps(n,arr));
+        }
+    }
+    public static int noOfJumps(int n,int []arr){
+        int count=0;
+        for(int i=0;i<n-1;i++,count++){
+            if(i+2<n && arr[i+2]==0){
+                i++;
+            }
+        }
+        return count;
+    }
+}
