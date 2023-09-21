1477465bd119c572597b5648a105ed8b343f111b
Aniket <anklet.devil09@gmail.com>
2023-09-21 19:22:17 +0530

    new file:   noddyExamination.java new file:   rideQueue.java

---
+++ noddyExamination.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class Main {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(noddyExamination(n,x,arr));
+    }
+    public static int noddyExamination(int n,int x,int arr[]){
+        int c1 = 0;
+        int c2 = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<=x){
+                c1++;
+            }else if(arr[i]>x){
+                c2++;
+            }if(c2>1){
+                break;
+            }
+        }
+        return c1;
+        
+    }
+}
---
+++ rideQueue.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class Main {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc-- > 0){
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(rideQueue(n,arr));
+        }
+    }
+    public static int rideQueue(int n ,int arr[]){
+        int count = 0;
+        for(int i=0;i<n;){
+            int j=i+1;
+            while(j<n && arr[j]>arr[j-1]){
+                j++;
+            }
+            count++;
+            i=j;
+        }
+        return count;
+    }
+}
